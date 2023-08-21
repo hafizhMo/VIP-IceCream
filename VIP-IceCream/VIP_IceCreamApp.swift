72ec79d706177d768dc99dc64d85ec8f3c7e4815
hafizhMo <hafizh.hizb@gmail.com>
2023-08-21 17:38:35 +0700

    Initial Commit

---
+++ VIP-IceCream/VIP_IceCreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIP_IceCreamApp.swift
+//  VIP-IceCream
+//
+//  Created by Hafizh Mo on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct VIP_IceCreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
